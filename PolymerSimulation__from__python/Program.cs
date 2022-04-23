376f8b4a5c0829cd96e32e377e2be1f406b5a9e9
pc <pc@EDURAZEE>
2022-04-23 17:15:15 +0200

    Add project files.

---
+++ PolymerSimulation__from__python/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymerSimulation__from__python
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
